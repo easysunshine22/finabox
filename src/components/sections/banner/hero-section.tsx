c893bb069668e64146c8ba28133ec893f32dc3e3
sunshine-web-coder <easysunshine22@gmail.com>
2025-08-01 16:53:39 +0100

    Update hero section text leading and adjust make alert section padding

--- src/components/sections/banner/hero-section.tsx
+++ src/components/sections/banner/hero-section.tsx
@@ -17,12 +17,12 @@ export default function HeroSection() {
         <div className="relative -top-[45px] sm:-top-[80px] md:-top-[90px] lg:-top-[110px] xl:-top-[128px] flex flex-col items-center justify-center">
           {/* <div className="absolute pb-15 -bottom-[180px] sm:-bottom-[220px] md:-bottom-[200px] lg:-bottom-[300px] xl:-bottom-[335px] left-0 right-0 flex flex-col items-center justify-center"> */}
           <FadeIn direction="up" delay={0.4} distance={60} duration={0.8}>
-            <h1 className="font-bold gradientTextColor text-[80px] sm:text-[128px] md:text-[128px] lg:text-[200px] xl:text-[256px] leading-[352px] text-center">
+            <h1 className="font-bold gradientTextColor text-[80px] sm:text-[128px] md:text-[128px] lg:text-[200px] xl:text-[256px] leading-[100%] lg:leading-[352px] text-center">
               Finabox
             </h1>
           </FadeIn>
 
-          <StaggeredFadeIn direction="up" initialDelay={0.6} staggerDelay={0.15} distance={20} duration={0.6} className="w-[306px] sm:w-[512px] flex flex-col gap-3 text-center">
+          <StaggeredFadeIn direction="up" initialDelay={0.6} staggerDelay={0.15} distance={20} duration={0.6} className="w-[306px] sm:w-[512px] gap-3 text-center">
             <h3 className="text-[20px] sm:text-[24px] mb-1 text-white leading-[24px] font-medium">The smart, secure inbox for your money</h3>
             <p className="text-[14px] sm:text-[16px] text-[#FFFFFF80] leading-[22px] sm:leading-[24px] font-[400]">
               Cut SMS costs, stay compliant, and deliver instant transaction messages — built for banks and their customers.
